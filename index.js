3f9b1821098027cda351cc8a9a9e4b5cfe8fc63c
Patrick Smyth <patricksmyth1990@proton.me>
2023-07-29 12:14:48 +0900

    Allow the user to change the number of squares using the button. Don't change anything if the user enters a number less than 16 or greater than 100.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// selects the "parent div" already in the HTML file
-const parentDiv = document.querySelector('div');
+let body = document.querySelector('body');
 
 // this function creates a grid of "child divs" within the parent div, depending on the number input
 function createGrid(number) {
+    // selects the "parent div" already in the HTML file
+    const parentDiv = document.getElementById('parentDiv');
     const fraction = 100/number;
     const strFraction = fraction.toString();
     for (let i = 0; i < number * number; i++) {
@@ -18,10 +19,8 @@ function createGrid(number) {
 
 // creates the initial 16 x 16 grid
 createGrid(16);
-
 // changes the color of any square when moused over
 let square = document.querySelectorAll('.square');
-
 square.forEach((item) => {
     item.addEventListener('mouseover', function() {
         item.setAttribute('class', 'hoveredOver');
@@ -30,14 +29,21 @@ square.forEach((item) => {
 
 // replaces the 16 x 16 grid with a new one with up to 100 x 100 squares, depending on the user's input on a prompt
 let button = document.querySelector('button');
-
 button.addEventListener('click', function() {
     const number = parseInt(prompt('How many squares do you want in the grid? You can enter a number between 16 and 100.'));
     if (number > 15 && number < 101) {
-        document.removeChild(parentDiv);
+        body.removeChild(parentDiv);
         const newGrid = document.createElement('div');
         newGrid.setAttribute('id', 'parentDiv');
-        document.insertBefore(newGrid, 'button');
+        body.insertBefore(newGrid, button);
         createGrid(number);
+    } else {
+        window.alert('Please try again and enter a number between 16 and 100.')
     };
+    square = document.querySelectorAll('.square');
+    square.forEach((item) => {
+        item.addEventListener('mouseover', function() {
+            item.setAttribute('class', 'hoveredOver');
+        });
+    });
 });
